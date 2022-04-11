049b148de695f61b8d956b021458fc93c155c630
weichen <w3ichen@gmail.com>
2022-04-11 09:21:24 -0600

    Server is completed

--- back-end/server.py
+++ back-end/server.py
@@ -10,6 +10,7 @@ server = Flask(__name__)
 APPS_PATH = "../apps"
 
 
+# GET: fetch all apps in `apps` folder
 @server.route("/apps", methods=["GET"])
 def apps():
     # Load and return all json files from `apps` folder
@@ -29,6 +30,51 @@ def apps():
     return jsonify(response), 200
 
 
+def run_code(code):
+    response = {}
+    # (1) Get user inputs as json
+    inputs = request.get_json(force=True)
+    with tempfile.NamedTemporaryFile(mode="w", suffix=".py") as code_file:
+        # (2) Write code to a temp file
+        code_file.write(code)
+        code_file.flush()
+        # (3) Capture stdout
+        stdout = io.StringIO()
+        with redirect_stdout(stdout):
+            # (4) Compile and run the app code
+            try:
+                # https://stackoverflow.com/a/67692
+                # Compile the functions
+                spec = importlib.util.spec_from_file_location("app_code", code_file.name)
+                script = importlib.util.module_from_spec(spec)
+                spec.loader.exec_module(script)
+                # Execute the main() function
+                response["outputs"] = clean_json(script.main(inputs))
+            except Exception as e:
+                response["error"] = traceback.format_exc()
+        response["stdout"] = stdout.getvalue()
+    return response
+
+
+# Handles TypeError: not JSON serializable
+def clean_json(dirty_json):
+    def set_default(obj):
+        import numpy as np
+        import pandas as pd
+
+        if isinstance(obj, (set, pd.core.series.Series, np.ndarray)):
+            return list(obj)  # convert list
+        else:
+            try:
+                return str(obj)  # As last resort, cast to string
+            except Exception as e:
+                raise TypeError
+
+    return json.loads(json.dumps(dirty_json, default=set_default))
+
+
+# GET: get a specific app with `app_id`
+# POST: run the code of the specific app with inputs and returns the outputs
 @server.route("/app/<string:app_id>", methods=["GET", "POST"])
 def app(app_id):
     try:
@@ -60,33 +106,6 @@ def app(app_id):
     elif request.method == "POST":
         response = run_code(data.get("code"))
     return jsonify(response), 200
-
-
-def run_code(code):
-    response = {}
-    # (1) Get user inputs as json
-    inputs = request.get_json(force=True)
-    with tempfile.NamedTemporaryFile(mode="w") as code_file:
-        # (2) Write code to a temp file
-        code_file.write(code)
-        # (3) Capture stdout
-        stdout = io.StringIO()
-        with redirect_stdout(stdout):
-            # (4) Compile and run the app code
-            try:
-                # https://stackoverflow.com/a/67692
-                # Compile the functions
-                spec = importlib.util.spec_from_file_location("app_module", code_file.name)
-                print("code_file.name", code_file.name)
-                print("spec:", spec)
-                script = importlib.util.module_from_spec(spec)
-                spec.loader.exec_module(script)
-                # Execute the main() function
-                response["outputs"] = script.main(inputs)
-            except Exception as e:
-                response["error"] = traceback.format_exc()
-        response["stdout"] = stdout.getvalue()
-    return response
 
 
 if __name__ == "__main__":
--- front-end/pages/_app.tsx
+++ front-end/pages/_app.tsx
@@ -1,11 +1,25 @@
 import { CssBaseline } from "@mui/material";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { SnackbarProvider } from "notistack";
 import Navbar from "../components/Navbar/Navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SnackbarProvider maxSnack={3} preventDuplicate>
+      <Head>
+        <title key="title">MecSimCalc</title>
+        <link key="favicon" rel="shortcut icon" href="/favicon.ico" />
+        <meta key="ogTitle" property="og:title" content="MecSimCalc" />
+        <meta key="ogImage" property="og:image" content="/logo2.png" />
+        <meta
+          key="ogDescription"
+          property="og:description"
+          content="The simplest way to build and share computational tools on the web. Create and share your Python apps in minutes using our intuitive and simple platform."
+        />
+        <meta key="ogUrl" property="og:url" content="mecsimcalc.com" />
+        <meta key="ogType" property="og:type" content="calculators" />
+      </Head>
       <CssBaseline />
       <Navbar />
       <Component {...pageProps} />
